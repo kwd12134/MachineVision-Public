3468786c7a8cba6468a3fc9faa149ba4f4343eb7
KWD <2597425737@qq.com>
2025-09-29 23:01:53 +0800

    Initial commit

---
+++ MachineVision/MachineVision.Defect/Models/RegionContextResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineVision.Defect.Models
+{
+   public  class RegionContextResult
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public string Name { get; set; }
+
+        public LightAndDarkRegion Render {  get; set; }
+
+        public RectangleLocation Location { get; set; }
+        
+    }
+}
